59271ae8c31e07b90409f9755e1aedbb9e0a11f1
Pappy <DPapworth@academytrainee.com>
2021-05-24 16:32:06 +0000

    basic fuctions done

--- animal_api/app.py
+++ animal_api/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
+import random 
 
 app = Flask(__name__)
 
 # animal generator route here
+@app.route('/get_animal')
+def get_animal():
+    return random.choice(['cow', 'horse', 'pig'])
 
-# animal noise generator route here
+@app.route('/get_noise', method=['POST'])
+def get_noise(animal):
+    noises = {
+        'cow' : 'moo'
+        'horse' : 'neigh'
+        'pig' : 'oink'
+    }
+    return noises[request.data.decode('utf-8')]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
